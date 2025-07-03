9c956ca4c824ca9370d33ef66e98664c876f355e
Rephael4321 <Rephael4321@gmail.com>
2025-07-03 10:58:14 +0300

    updated navbar image

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -11,11 +11,11 @@ export default function Navbar({ isAdmin = false }: { isAdmin?: boolean }) {
         className="flex items-center gap-2 text-xl sm:text-2xl font-bold text-pink-700 whitespace-nowrap"
       >
         <Image
-          src="/images/RAW/גלידות חיים/IMG_8444 fimmm.png" // <- replace with your actual image path
+          src="/favicon_io/android-chrome-192x192.png"
           alt="המפנק"
           width={32}
           height={32}
-          className="object-contain"
+          sizes="(max-width: 640px) 32px, (max-width: 1024px) 64px, 128px"
         />
         המפנק
       </Link>
