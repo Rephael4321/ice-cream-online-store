b5934bc2993a2176d85307e65885103af93698fd
Rephael4321 <Rephael4321@gmail.com>
2025-07-24 18:12:33 +0300

    added go back button in categories list

--- src/components/cms/entities/category/list.tsx
+++ src/components/cms/entities/category/list.tsx
@@ -4,12 +4,16 @@ import { useState } from "react";
 import ViewCategories from "./ui/view-categories";
 import OrganizeCategories from "./ui/organize-categories";
 import { Button } from "@/components/cms/ui/button";
+import Link from "next/link";
 
 export default function ListCategories() {
   const [mode, setMode] = useState<"view" | "organize">("view");
 
   return (
     <div className="px-4 sm:px-6 md:px-10 max-w-7xl mx-auto">
+      <Link href="/cms" className="text-blue-600 hover:underline">
+        ← חזרה לניהול
+      </Link>
       <h1 className="text-2xl font-bold mb-4 text-purple-700 text-center">
         ניהול קטגוריות
       </h1>
